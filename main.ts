0c15dc72c748dd6974a377da2bfe17831c38f769
Seth0713 <jb.giuliana@protonmail.com>
2022-06-27 08:55:33 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,15 +4,16 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendNumber(3)
+    stop = 0
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
+    stop = 0
 })
 let stop = 0
 radio.setGroup(1)
 basic.forever(function () {
     if (stop == 2) {
         radio.sendNumber(4)
-        stop = 0
     }
 })
